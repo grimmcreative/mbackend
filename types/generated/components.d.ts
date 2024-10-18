c439e59c4e33da86b8dcb711b71873b038108f18
Maximilian Grimm <grimm@grimmcreative.com>
2024-10-18 17:44:21 +0200

    create testcomponent

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,20 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface TestTestcomponent extends Schema.Component {
+  collectionName: 'components_test_testcomponents';
+  info: {
+    displayName: 'testcomponent';
+    icon: 'alien';
+  };
+  attributes: {
+    justatest: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'test.testcomponent': TestTestcomponent;
+    }
+  }
 }
